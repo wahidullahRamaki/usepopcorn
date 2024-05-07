c0c6cfa7b97f995067867bc08025c65276e090ac
wahidullahRamaki <30998117+wahidullahRamaki@users.noreply.github.com>
2024-05-07 09:19:39 +0430

    Apply PropTypes on Props

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -9,7 +10,15 @@ const containerStyle = {
 const starContainerStyle = {
   display: "flex",
 };
-
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  message: PropTypes.array,
+  onSetRating: PropTypes.func,
+  className: PropTypes.string,
+};
 export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
