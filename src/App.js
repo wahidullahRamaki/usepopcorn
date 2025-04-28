059d051de8f327fb623183c63643ec5970240258
wahidullahRamaki <30998117+wahidullahRamaki@users.noreply.github.com>
2025-04-28 11:12:45 +0430

    save data to localstorage

--- src/App.js
+++ src/App.js
@@ -27,13 +27,21 @@ function handleCloseMovie(){
 function handleAddWatched(movie){
    setWatched((watched)=>[...watched, movie]);
 
+  //  localStorage.setItem('watched', JSON.stringify([...watched, movie]));
+
 }
 
 function handleDeleteWatched (id) {
 setWatched( watched=>watched.filter(movie=>movie.imdbID !== id))
 }
 
+  useEffect(function(){
 
+    localStorage.setItem('watched', JSON.stringify(watched));
+  },[watched])
+
+
+  
   useEffect(function(){
  const controller = new AbortController();  
     async function fetchMovies() {
